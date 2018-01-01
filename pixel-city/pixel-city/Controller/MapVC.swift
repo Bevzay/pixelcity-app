9004d6c9db46650949c2471ae0cd1e13a0552958
Beverly <beverly@Neils-MacBook-Pro.local>
2018-01-01 17:39:00 +1300

    Project Setup

---
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Beverly on 1/01/18.
+//  Copyright © 2018 Beverly. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
